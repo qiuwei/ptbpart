bce660eba8061b498b8573a3d5e48151dc2ccf7c
Wei Qiu <wei.qiu@uni-tuebingen.de>
2014-07-21 14:29:55 +0200

    add file extention filter

--- src/main/java/es/qiu/ptbpart/Partitioner.java
+++ src/main/java/es/qiu/ptbpart/Partitioner.java
@@ -18,11 +18,11 @@ public class Partitioner {
     //create an empty InputStream
     private static InputStream dirStream = new ByteArrayInputStream(new String("").getBytes());
 
-    public static void partition(Integer numPartition, String fileOrDirName, String outputName) throws IOException {
+    public static void partition(Integer numPartition, String fileOrDirName, String outputName, String extension) throws IOException {
         File f = new File(fileOrDirName);
         PennTreeReader ptReader = null;
         if(f.isDirectory()) {
-            ptReader = loadPTBCorpusFromDir(fileOrDirName);
+            ptReader = loadPTBCorpusFromDir(fileOrDirName, extension);
         }else{
             ptReader = loadPTBCorpusFromFile(fileOrDirName);
         }
@@ -56,17 +56,17 @@ public class Partitioner {
         return ptReader;
     }
 
-    private static PennTreeReader loadPTBCorpusFromDir(String dirName){
+    private static PennTreeReader loadPTBCorpusFromDir(String dirName, String extension){
         PennTreeReader ptReader = null;
         File dir = new File(dirName);
         assert(dir.isDirectory());
-        transverse(dir);
+        transverse(dir, extension);
         return new PennTreeReader(new InputStreamReader(dirStream));
     }
 
-    private static void transverse(File node){
-        System.out.println("accessing " + node.getName());
-        if(node.isFile()){
+    private static void transverse(File node, String extension){
+        if(node.isFile() && node.getName().endsWith(extension)){
+            System.out.println("accessing " + node.getName());
             try {
                 dirStream = new SequenceInputStream(dirStream, new FileInputStream(node));
             } catch (FileNotFoundException e) {
@@ -76,7 +76,7 @@ public class Partitioner {
         else if(node.isDirectory()){
             String[] subNodes = node.list();
             for (String fileName : subNodes){
-                transverse(new File(node, fileName));
+                transverse(new File(node, fileName), extension);
             }
         }
     }
@@ -89,11 +89,12 @@ public class Partitioner {
         ArgumentParser parser = ArgumentParsers.newArgumentParser("PtPartition").description("partition Penn TreeBank style corpus into several parts");
         parser.addArgument("num").metavar("N").type(Integer.class).help("Number of partitions desired");
         parser.addArgument("path").metavar("P").help("The path to the file or directory which contains the corpus");
+        parser.addArgument("--ext").metavar("E").setDefault(".mrg").help("only file with certain extension will be processed");
         parser.addArgument("--outname").metavar("O").setDefault("partition").help("the prefix used for names of output files");
         try {
             Namespace res = parser.parseArgs(args);
             //partition(10, "/Users/wqiu/Projects/eval/data/penn-treebank/original/CD/combined/wsj/00/wsj_0002.mrg");
-            partition((Integer)res.get("num"), (String)res.get("path"), (String)res.get("outname"));
+            partition((Integer)res.get("num"), (String)res.get("path"), (String)res.get("outname"), (String)res.get("ext"));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ArgumentParserException e) {
